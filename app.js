79c891431c67857fdee73a803dd868be1918039e
neto <yo@ernestoaraiza.com>
2018-03-11 00:25:42 -0500

    bad json, cleaning it, adding error to getjson

---
+++ app.js
@@ -0,0 +1,31 @@
+var vm = new Vue({
+            el: '#vue-orgs',
+            data:{
+                question:"Pregunta 1",
+                survey:null,
+              },
+            created: function() {
+                  console.log("created!");
+                  this.getJson();
+                },
+            methods: {
+                getJson: function(){
+                  console.log("in getJson!");
+                  var _this = this;
+                  $.getJSON('survey_results.json',function(res){
+                    console.log("in getJson de jQuery!");
+                    console.log(res);
+                    _this.survey = res;
+                  }).done(function() {
+                    console.log( "second success" );
+                  })
+                  .fail(function( jqxhr, textStatus, error ) {
+                      var err = textStatus + ", " + error;
+                      console.log( "Request Failed: " + err );
+                  })
+                  .always(function() {
+                    console.log( "complete" );
+                  });
+              }
+            }
+        });

ae7c0a25c4a4cc2061ba1ac0c3eba6f7e07c7bdc
neto <yo@ernestoaraiza.com>
2018-03-11 15:54:21 -0400

    highlights only one occurrence

--- js/app.js
+++ js/app.js
@@ -18,7 +18,8 @@ var vm = new Vue({
                 findText(){
                   console.log("find: "+this.search);
                   livesearch = this.search;
-
+                  this.unhighlightAll("1. Since the adoption of the 2030 Agenda and the SDGs, has the governing body of your organization taken (or will it take) any decisions or new strategies to guide the implementation of the 2030 Agenda and the SDGs? If any, please provide a brief summary below, including the overarching vision of your organization.");
+                  //finds all answers that cointain that question
                   answers = this.surveyQJson.find("1. Since the adoption of the 2030 Agenda and the SDGs, has the governing body of your organization taken (or will it take) any decisions or new strategies to guide the implementation of the 2030 Agenda and the SDGs? If any, please provide a brief summary below, including the overarching vision of your organization.",
                             function(){
                               var re=new RegExp(livesearch,"gi");
@@ -27,18 +28,42 @@ var vm = new Vue({
                             }).parent();
                   //console.log(answers);
                   this.hideAll();
+                  //the code below shows the answers where the keyword is present
                   _this = this;
                   answers.each(function (index, path, value) {
-                      console.log(value.Organization);
-                      console.log(value["Respondent ID"]);
+                      //console.log(value.Organization);
+                      //console.log(value["Respondent ID"]);
                       _this.showId(value["Respondent ID"]);
+                      _this.highlightId(value["Respondent ID"],
+                        "1. Since the adoption of the 2030 Agenda and the SDGs, has the governing body of your organization taken (or will it take) any decisions or new strategies to guide the implementation of the 2030 Agenda and the SDGs? If any, please provide a brief summary below, including the overarching vision of your organization.",
+                        livesearch);
                   });
-                  //return this.sibling('Organization')
+
+
                 },
                 showId: function(id){
                   _.each(this.survey, function(entry){
                     if (id==entry["Respondent ID"]){
                       entry.show=1;
+                    }
+                  });
+                },
+                unhighlightAll: function(question){
+                  _.each(this.survey, function(entry){
+                    //console.log(entry[question]);
+
+                      console.log(entry[question]);
+                      entry[question] = entry[question].replace('<span class="highlight">','');
+                      entry[question] = entry[question].replace('<\/span>','');
+                  });
+                },
+                highlightId: function(id, question, word){
+                  _.each(this.survey, function(entry){
+                    if (id==entry["Respondent ID"]){
+                      //todo use regexp instead
+                      //entry.show=1;
+                      entry[question] = entry[question].replace(word,'<span class="highlight">'+word+'</span>');
+                      //console.log(entry[question]);
                     }
                   });
                 },

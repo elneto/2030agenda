e114f3a1a63ba263230d2a25181af2a52aff7015
Ernesto <araiza@un.org>
2018-04-20 12:11:13 -0400

    removed comments from app.js

--- js/app.js
+++ js/app.js
@@ -25,19 +25,14 @@ var vm = new Vue({
     this.getExcerptsJson();
   },
   methods: {
-    filterOrg(id) {
-      //console.log("start filterOrg " + id);
+    filterOrg(id) { //shows only the clicked button
       this.findOccurrences();
-      //this.$forceUpdate();
       this.orderedEntries.forEach(function(entry) {
         if (entry.id == id)
           entry.showO = 1;
         else
           entry.showO = 0;
       });
-      // this.findOccurrences();
-      //this.$forceUpdate();
-      //console.log("finished filterOrg " + id);
     },
     clearSearch() {
       this.search = "";
@@ -55,13 +50,10 @@ var vm = new Vue({
       return this.questions[q];
     },
     findOccurrences() {
-      //this.hideQuestions();
       this.clearExcerpts();
       //todo trim this.search?
       if (this.search === '') {
-        //todo change 8 to totalObjects in array
         this.orderedEntries.forEach(function(entry) {
-          //console.log("entry.sorto "+entry.sorto);
           entry.sorto = 0;
           entry.showO = 1;
         });
@@ -71,58 +63,38 @@ var vm = new Vue({
 
       livesearch = this.search;
       var grandTotal = 0;
-      for (var i=0; i < this.survey.length; i++) { //for each entry
+      for (var i = 0; i < this.survey.length; i++) { //for each entry
         var totalOrg = 0;
-        //console.log("exceprt i:"+this.excerpt[i].Short);
         this.excerpt[i].sorto = totalOrg;
-        var _this = this;
         for (var key in this.survey[i]) {
-          if (key.indexOf("q") != -1 || key.indexOf("docs") != -1){
-            //entry has the question or key and entry[answer] the actual answer
-            // console.log("key: "+key);
-            // console.log("answer: "+this.survey[i][key]);
+          if (key.indexOf("q") != -1 || key.indexOf("docs") != -1) {
+
             let answer = this.survey[i][key];
             var arr = String(answer).match(new RegExp(livesearch, "gi"));
             if (arr) {
-              //arr.length has the number of occurrences of the term searched in this answer
-              //$('#answer' + entry.id + answer).show();
 
               var left_arr = String(answer).match(new RegExp("(\\S+\\s+){0,7}" + livesearch, "im"));
               var right_arr = String(answer).match(new RegExp(livesearch + "[^\.;]+", "i"));
-              //entry[answer] = "";
+
               if (left_arr && right_arr) {
                 var left = left_arr[0].substring(0, left_arr[0].length - livesearch.length);
                 var right = right_arr[0].substring(livesearch.length);
-                //$('#answertext' + entry.id + answer).html("..." + left + " <strong>" + livesearch + "</strong>" + right);
-                //console.log("this.excerpt[i][key]: "+this.excerpt[i][key]);
                 this.excerpt[i][key] = "..." + left + " <strong>" + livesearch + "</strong>" + right;
-                //console.log("I found "+ "..."+left +livesearch +right + " IN ENTRY " + entry.id +answer);
               }
               totalOrg += arr.length;
               this.excerpt[i].sorto = totalOrg;
-              //console.log(entry.Short+" sorto = "+entry.sorto);
             }
           }
         }
-        //console.log(entry.Short + " " + totalOrg);
         grandTotal += totalOrg;
-        //$('#badge'+entry.id).text(totalOrg);
-        // if (entry.sorto>0){
-        //   $('#btn'+entry.id).removeClass('btn-secondary').addClass('btn-success');
-        // }
-        // else{
-        //   $('#btn'+entry.id).removeClass('btn-success').addClass('btn-secondary');
-        // }
-        //console.log('total '+grandTotal)
+
         $('#textTotal').text(grandTotal);
       } //end foreach
-      //this.$forceUpdate();
     },
     clearExcerpts() {
       this.orderedEntries.forEach(function(entry) {
-        for (var k in entry){
+        for (var k in entry) {
           if (entry.hasOwnProperty(k) && (k.indexOf("q") != -1 || k.indexOf("docs") != -1)) {
-            //console.log("Key is " + k + ", value is" + entry[k]);
             entry[k] = "";
           }
         }
